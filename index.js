d30e3b36a1835468abc92a2f2050e2ce8b970078
Ash Dan <Arvolley01@gmail.com>
2022-07-05 12:30:11 -0400

    commit tree c9db7a643fbc5bf3f43d8b002a96355c8ee20445

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+
+const submitData = (name, email) => {
+    const f = fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    }).then((res) => res.json()).then((json) => {
+        document.body.append(json.id)
+    }).catch((error) => {
+        document.body.append(error.message)
+    });
+
+    return f;
+}
